0d535c2bdba67debb6be3e924f3a27801ed9d23c
Kelly Bowman <KBowman5@CNM.Edu>
2022-09-16 19:52:04 -0600

    Got all three sub-cat on one but run will not work when all as one

--- Objects, Interfaces, and API's.js
+++ Objects, Interfaces, and API's.js
@@ -2,7 +2,51 @@
  * Conditionals, Functions, Scope and Loops.
  */
 
+// Equals
+let equals = 1 === 1;
+console.log(equals);
 
+// Greater than
+let greaterThan = 5 > 1;
+
+// Less than
+let lessThan = 2 < 10;
+
+// Greater than Equals
+let greaterThanEq = 5 >= 5;
+
+// Less than Equals
+let lessThanEq = 4 <= 9;
+
+// Not Equals
+let notEquals = 5 !== 2;
+
+let storeA = 1.40
+let storeB = 3.40
+
+function  compareStorePrices (storeA, storeB) {
+    let storeAIsLower = storeA < storeB;
+    if (storeAIsLower) {
+        console.log("Store A has a lower price.")
+    } else if (storeB < storeA) {
+        console.log("Store B has a lower price.")
+    } else {
+        console.log("Their prices are equal.")
+    }
+}
+
+//compareStorePrices(10, 5);
+//compareStorePrices(7, 10);
+
+function squareNum (number) {
+    let squared = number + number;
+    return squared;
+}
+
+
+
+let squaredNumber = squareNum(10);
+console.log(squaredNumber);
 
 let x = 10;
 
@@ -18,7 +62,7 @@ function addNumbers (n, m, x) {
 
 addNumbers(1, 2, 10);
 
-/*...*/
+
 
 //              0  1  2  3  4  5  6
 let ourArray = [1, 2, 3, 4, 5, 6, 7];
