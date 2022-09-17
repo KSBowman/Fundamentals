7049b3508d529e51827ed15d890409eb9c01c12b
Kelly Bowman <KBowman5@CNM.Edu>
2022-09-16 18:07:02 -0600

    Got all three sub-cat on one but run will not work when all as one

--- Variable and Types.js
+++ Variable and Types.js
@@ -1,6 +1,27 @@
 /*
  * Variables, Data Types, and Typing
  */
+
+let wordData;
+
+wordData = "Word list";
+
+Console.log(wordData);
+
+let subTotal = 100
+let tac = subTotal * 0.07;
+let totalWithTax = tax = subTotal;
+
+let obj = {
+    key1: "Value 1",
+    key2: 3,
+    obj2: {
+        obj2Key1 : "value or inner object",
+    }
+}
+
+Console.log(obj.obj2.obj2Key1);
+
 let add = 1 + 2;
 let sub = 2 - 1;
 let mult = 2*4;
