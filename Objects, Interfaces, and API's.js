ffed959c06dda3fd36e96d894480d3f534a06b78
Kelly Bowman <KBowman5@CNM.Edu>
2022-09-19 13:54:34 -0600

    All three subs complete

--- Objects, Interfaces, and API's.js
+++ Objects, Interfaces, and API's.js
@@ -7,9 +7,19 @@ let dog = {
     color: "Brown",
     breed: "Pug",
     size: "Small",
-    bark: function(){
+    bark: function(typeOfBark){
         console.log("Bark!");
     },
 };
 
-dog.bark();
+function  x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "Tom",
+    num: 10,
+}
+x(y);
+console.log(y);
